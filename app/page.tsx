1dea83be4e08190414495c3024bb9c057e487f73
JonathanWaller <waller.j27@gmail.com>
2024-04-02 15:33:10 -0500

    links main page to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <h1>Task Flow</h1>
-        <button className={styles.enter}>Enter</button>
+        <Link
+          href={'/dashboard'}
+        >
+          <button className={styles.enter}>Enter</button>
+        </Link>
         {/* <div className={styles.enter-btn}>Enter</div> */}
         {/* <div className={styles.enter}>yo</div> */}
       </div>
