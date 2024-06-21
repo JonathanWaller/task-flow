151474c5480dd6f6bfef334b9ba38b9d5fff0ebe
JonathanWaller <waller.j27@gmail.com>
2024-06-21 16:23:55 -0500

    Mapping utils

--- app/lib/utils.tsx
+++ app/lib/utils.tsx
@@ -7,6 +7,20 @@ export const boardColumns: Column[] = [
     { category: 'complete', display: 'Complete'}
 ]
 
+export const statusMapping: any = {
+  backlog: 'Backlog',
+  todo: 'To Do',
+  dev: 'In Development',
+  complete: 'Complete'
+}
+
+export const statusColorMapping: any = {
+  backlog: 'pink',
+  todo: 'red',
+  dev: 'blue',
+  complete: 'green'
+}
+
 export const modalStyles = {
     content: {
       top: '50%',
