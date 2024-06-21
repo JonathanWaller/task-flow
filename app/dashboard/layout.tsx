538af9f686d6005d1865b69a2e60a2ecb9f953bd
JonathanWaller <waller.j27@gmail.com>
2024-06-21 16:23:42 -0500

    Adds ticket context

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,27 @@
+'use client';
+
 // import SideNav from '@/app/ui/dashboard/sidenav';
 import Sidebar from "../ui/sidebar";
 import { SidebarProvider } from "../context/SidebarContext";
+
+import { createContext, useContext, useState } from "react";
  
+
+export const CurrentTicketContext = createContext<any>(null);
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+
+  // const [ currentTicket, setCurrentTicket ] = useState<any>(null);
+  const [ currentTicket, setCurrentTicket ] = useState<any>({name: 'betty bop'});
+
   return (
     <SidebarProvider>
+      <CurrentTicketContext.Provider
+        value={{
+          currentTicket,
+          setCurrentTicket
+        }}
+      >
       <div style={{display: 'flex'}}>
         <div>
           <Sidebar />
@@ -14,6 +31,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         </div>
         <div>{children}</div>
       </div>
+      </CurrentTicketContext.Provider>
     </SidebarProvider>
   );
 }
